8376e249375c6ec350b14460d99325cc88392b30
Rizwan <alirizwan24188@gmail.com>
2026-09-18 23:16:29 +0530

    api user

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,23 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const usersRouter = require('./users');
+const usersRouter = require(".api/users");
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://shama-chicken-shop.vercel.app"
-  ]
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://shama-chicken-shop.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    credentials: true
+  })
+);
 
-app.use('/api/users', usersRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
