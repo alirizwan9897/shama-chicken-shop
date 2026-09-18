2f04b1f14f31e3ac3f048cbb51738ec31c646216
Rizwan <alirizwan24188@gmail.com>
2026-09-18 22:50:23 +0530

    backend changes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
-const usersRouter = require('./api/users');
+
+const usersRouter = require('./users');
+
 const app = express();
-const port = Number(process.env.PORT || 4000);
+
 app.use(express.json());
+
 app.use(cors({
-  origin: "http://localhost:3000"
+  origin: [
+    "http://localhost:3000",
+    "https://shama-chicken-shop.vercel.app"
+  ]
 }));
+
 app.use('/api/users', usersRouter);
-app.listen(port, () => {
-  console.log(`API server running on http://localhost:${port}`);
-});
+
+module.exports = app;
